81344faa80b2a41a65682b8c1b8f8fe5b6950965
Dallas Hart <nomalahvids@gmail.com>
2022-10-12 17:37:43 -0400

    Make working motor controller reciever

--- src/CANLib/Tools/converters.hpp
+++ src/CANLib/Tools/converters.hpp
@@ -20,3 +20,75 @@ float convertToUnsigned(uint8_t low_byte, uint8_t high_byte) {
     uint16_t combined = combineTo<uint16_t>(low_byte, high_byte);
     return static_cast<float>(combined) / ratio;
 }
+
+float toCelsius(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+float toLowVoltage(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<100>(low_byte, high_byte);
+}
+
+float toNm(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+float toHighVoltage(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+float toAmps(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+float toDegrees(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+int16_t toRPM(std::byte low_byte, std::byte high_byte) {
+    return combineTo<int16_t>(low_byte, high_byte);
+}
+
+bool toBool(std::byte byte) {
+    return static_cast<bool>(byte);
+}
+
+float toHz(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+float tokW(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
+
+float toWebers(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<1000>(low_byte, high_byte);
+}
+
+// either 100 or 10000 scale
+float toProportionalGain1(std::byte low_byte, std::byte high_byte) {
+    return convertToUnsigned<100>(low_byte, high_byte);
+}
+float toProportionalGain2(std::byte low_byte, std::byte high_byte) {
+    return convertToUnsigned<10000>(low_byte, high_byte);
+}
+
+float toIntegralGain(std::byte low_byte, std::byte high_byte) {
+    return convertToUnsigned<10000>(low_byte, high_byte);
+}
+
+float toDerivativeGain(std::byte low_byte, std::byte high_byte) {
+    return convertToUnsigned<100>(low_byte, high_byte);
+}
+
+float toLowpassFilterGain(std::byte low_byte, std::byte high_byte) {
+    return convertToUnsigned<10000>(low_byte, high_byte);
+}
+
+uint16_t toCount(std::byte low_byte, std::byte high_byte) {
+    return combineTo<uint16_t>(low_byte, high_byte);
+}
+
+float toPSI(std::byte low_byte, std::byte high_byte) {
+    return convertToSigned<10>(low_byte, high_byte);
+}
--- src/Interfaces/Full/MotorController.cpp
+++ src/Interfaces/Full/MotorController.cpp
@@ -2,75 +2,71 @@
 
 #include <MotorController.hpp>
 #include <tools.hpp>
+#include <converters.hpp>
 
 using namespace CAN;
 
 void MotorController::newFrame(const can_frame& frame) {
-    fmt::print("ID: 0x{:02X}, Ext: {}, RTR: {}, Err: {}, Payload: 0x{:02X} 0x{:02X} 0x{:02X} "
-               "0x{:02X} 0x{:02X} 0x{:02X} 0x{:02X} 0x{:02X}\n",
-               CAN::frameId(frame),
-               CAN::frameFormat(frame) == CanFormat::Extended,
-               CAN::isError(frame),
-               frame.data[0],
-               frame.data[1],
-               frame.data[2],
-               frame.data[3],
-               frame.data[4],
-               frame.data[5],
-               frame.data[6],
-               frame.data[7]);
-
     switch (CAN::frameId(frame)) {
-    case 0x0A0: // Temps #1
-        break;
+        // case 0x0A0: // Temps #1
+        //     break;
 
-    case 0x0A1: // Temps #2
-        break;
+        // case 0x0A1: // Temps #2
+        //     break;
 
-    case 0x0A2: // Temps #3 & Torque Shudder
+    case 0x0A2: { // Temps #3 & Torque Shudder
+        fmt::print("Got Temps #3\n");
+        auto coolant_temp = toCelsius(frame.data[0], frame.data[1]);
+        fmt::print("Coolant Temp: {} C\n", coolant_temp);
+        emit newCoolantTemp(coolant_temp);
+        fmt::print("Hot Spot Temp: {} C\n", toCelsius(frame.data[2], frame.data[3]));
+        fmt::print("Motor Temp: {} C\n", toCelsius(frame.data[4], frame.data[5]));
+        fmt::print("Torque Shudder: {} Nm\n", toNm(frame.data[6], frame.data[7]));
         break;
+    }
 
-    case 0x0A3: // Unused
-        break;
+              // case 0x0A3: // Analog Status
+              //     break;
 
-    case 0x0A4: // Digital Input Status
+    case 0x0A4: { // Digital Input Status
+        fmt::print("Got Digital Input\n");
+        fmt::print("Forward: {}\n", toBool(frame.data[0]));
+        fmt::print("Reverse: {}\n", toBool(frame.data[1]));
+        fmt::print("Brake: {}\n", toBool(frame.data[2]));
+        fmt::print("Regen Disable: {}\n", toBool(frame.data[3]));
+        fmt::print("Ignition: {}\n", toBool(frame.data[4]));
+        fmt::print("Start: {}\n", toBool(frame.data[5]));
+        fmt::print("Valet: {}\n", toBool(frame.data[6]));
+        fmt::print("Input 8: {}\n", toBool(frame.data[7]));
         break;
+    }
 
-    case 0x0A5: // Motor Position Information
+    case 0x0A5: { // Motor Position Information
+        fmt::print("Got Motor Pos Info\n");
+        fmt::print("Elec Motor Angle: {} deg\n", toDegrees(frame.data[0], frame.data[1]));
+        fmt::print("Motor RPM: {} rpm\n", toRPM(frame.data[2], frame.data[3]));
+        fmt::print("Elec Out Freq: {} Hz\n", toHz(frame.data[4], frame.data[5]));
+        fmt::print("Delta Resolver: {} deg\n", toDegrees(frame.data[6], frame.data[7]));
+        emit newRPM(toRPM(frame.data[2], frame.data[3]));
         break;
+    }
 
-    case 0x0A6: //  Current Information
-        break;
-
-    case 0x0A7: //  Current Information
-        break;
-
-    case 0x0A8: //  Current Information
-        break;
-
-    case 0x0A9: //  Current Information
-        break;
-
-    case 0x0AA: //  Current Information
-        break;
-
-    case 0x0AB: //  Current Information
-        break;
-
-    case 0x0AC: //  Current Information
-        break;
-
-    case 0x0AD: //  Current Information
-        break;
-
-    case 0x0AE: //  Current Information
-        break;
-
-    case 0x0AF: //  Current Information
-        break;
-
-    case 0x0B0: // Fault Codes
-        break;
+              // case 0x0B0: // Fault Codes
+              //     break;
+    default:
+        fmt::print("ID: 0x{:02X}, Ext: {}, RTR: {}, Err: {}, Payload: 0x{:02X} 0x{:02X} 0x{:02X} "
+            "0x{:02X} 0x{:02X} 0x{:02X} 0x{:02X} 0x{:02X}\n",
+            CAN::frameId(frame),
+            CAN::frameFormat(frame) == CanFormat::Extended,
+            CAN::isError(frame),
+            frame.data[0],
+            frame.data[1],
+            frame.data[2],
+            frame.data[3],
+            frame.data[4],
+            frame.data[5],
+            frame.data[6],
+            frame.data[7]);
     }
 }
 
@@ -78,4 +74,4 @@ void MotorController::newError(const can_frame&) {
     fmt::print("Error\n");
 };
 
-void MotorController::newTimeout(){};
+void MotorController::newTimeout() {};
