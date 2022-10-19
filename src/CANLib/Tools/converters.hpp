e27b4b984ddc1baeff1077058842b31064e79964
Dallas <nomalahvids@gmail.com>
2022-10-19 11:12:36 -0400

    Improve CMake

--- src/CANLib/Tools/converters.hpp
+++ src/CANLib/Tools/converters.hpp
@@ -21,74 +21,23 @@ float convertToUnsigned(uint8_t low_byte, uint8_t high_byte) {
     return static_cast<float>(combined) / ratio;
 }
 
-float toCelsius(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-float toLowVoltage(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<100>(low_byte, high_byte);
-}
-
-float toNm(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-float toHighVoltage(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-float toAmps(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-float toDegrees(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-int16_t toRPM(std::byte low_byte, std::byte high_byte) {
-    return combineTo<int16_t>(low_byte, high_byte);
-}
-
-bool toBool(std::byte byte) {
-    return static_cast<bool>(byte);
-}
-
-float toHz(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-float tokW(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
-
-float toWebers(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<1000>(low_byte, high_byte);
-}
+float toCelsius(uint8_t low_byte, uint8_t high_byte);
+float toLowVoltage(uint8_t low_byte, uint8_t high_byte);
+float toNm(uint8_t low_byte, uint8_t high_byte);
+float toHighVoltage(uint8_t low_byte, uint8_t high_byte);
+float toAmps(uint8_t low_byte, uint8_t high_byte);
+float toDegrees(uint8_t low_byte, uint8_t high_byte);
+int16_t toRPM(uint8_t low_byte, uint8_t high_byte);
+bool toBool(uint8_t byte);
+float toHz(uint8_t low_byte, uint8_t high_byte);
+float tokW(uint8_t low_byte, uint8_t high_byte);
+float toWebers(uint8_t low_byte, uint8_t high_byte);
 
 // either 100 or 10000 scale
-float toProportionalGain1(std::byte low_byte, std::byte high_byte) {
-    return convertToUnsigned<100>(low_byte, high_byte);
-}
-float toProportionalGain2(std::byte low_byte, std::byte high_byte) {
-    return convertToUnsigned<10000>(low_byte, high_byte);
-}
-
-float toIntegralGain(std::byte low_byte, std::byte high_byte) {
-    return convertToUnsigned<10000>(low_byte, high_byte);
-}
-
-float toDerivativeGain(std::byte low_byte, std::byte high_byte) {
-    return convertToUnsigned<100>(low_byte, high_byte);
-}
-
-float toLowpassFilterGain(std::byte low_byte, std::byte high_byte) {
-    return convertToUnsigned<10000>(low_byte, high_byte);
-}
-
-uint16_t toCount(std::byte low_byte, std::byte high_byte) {
-    return combineTo<uint16_t>(low_byte, high_byte);
-}
-
-float toPSI(std::byte low_byte, std::byte high_byte) {
-    return convertToSigned<10>(low_byte, high_byte);
-}
+float toProportionalGain100(uint8_t low_byte, uint8_t high_byte);
+float toProportionalGain10000(uint8_t low_byte, uint8_t high_byte);
+float toIntegralGain(uint8_t low_byte, uint8_t high_byte);
+float toDerivativeGain(uint8_t low_byte, uint8_t high_byte);
+float toLowpassFilterGain(uint8_t low_byte, uint8_t high_byte);
+uint16_t toCount(uint8_t low_byte, uint8_t high_byte);
+float toPSI(uint8_t low_byte, uint8_t high_byte);
