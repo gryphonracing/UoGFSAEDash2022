99f6517a741af6a4304f65c82ba444b23c02a600
Dallas <nomalahvids@gmail.com>
2022-10-19 21:25:43 -0400

    restored cases for listening to CAN messages.

--- src/Interfaces/Full/BMS.cpp
+++ src/Interfaces/Full/BMS.cpp
@@ -47,26 +47,51 @@ void BMS::newFrame(const can_frame& frame) {
     //For now, just need the pack voltage and pack state of charge
     switch (CAN::frameId(frame)) {
 
-    //Pack voltage
-    // case 0xF00D: { 
-    //     // auto pack_votage = toVoltage(frame.data[3], frame.data[5]);
-    //     fmt::print("Pack votage: {} C\n", pack_votage);
-    //     //emit newCoolantTemp(coolant_temp);
-    //     break;
-    // }
+    //For now, just need the pack voltage and pack state of charge
+    switch (CAN::frameId(frame)) {
 
-              
-    // //Pack state of charge
-    // case 0xF00F: { // Digital Input Status
-    //     auto state_of_charge = toStateOfCharge(frame.data[3], frame.data[5]);
-    //     fmt::print("Pack state of charge (0-100): {} C\n", state_of_charge);
-    //     //emit newCoolantTemp(coolant_temp);
-    //     break;
-    // }
+    //Pack voltage
+    case 0xF00D: {
+        auto pack_votage = toVoltage(frame.data[3], frame.data[5]);
+        fmt::print("Pack votage: {} C\n", pack_votage);
+        //emit newCoolantTemp(coolant_temp);
+        break;
+    }
+
+
+    //Pack state of charge
+    case 0xF00F: { // Digital Input Status
+        auto state_of_charge = toStateOfCharge(frame.data[3], frame.data[5]);
+        fmt::print("Pack state of charge (0-100): {} C\n", state_of_charge);
+        //emit newCoolantTemp(coolant_temp);
+        break;
+    }
+
+    //Pack voltage and pack state of charge
+    case 0x0C0: {
+        auto pack_votage = toVoltage(frame.data[3], frame.data[4]);
+        auto state_of_charge = toStateOfCharge(frame.data[5], frame.data[6]);
+        fmt::print("Success A");
+    }
+
+    //Pack current and heat sink temp (celcius)
+    case 0x0C1: {
+        auto pack_current = toCurrent(frame.data[3], frame.data[4]);
+        auto temperature = toTemperature(frame.data[5], frame.data[6]);
+        fmt::print("Success A");
+    }
+
+    //Pack peak temp, lowest temp, and heat sink sink temp (celcius)
+    case 0x0C2: {
+        auto temperature_heat_sink = toTemperature(frame.data[3], frame.data[4]);
+        auto temperature_high = toTemperature(frame.data[5], frame.data[6]);
+        auto temperature_low = toTemperature(frame.data[7], frame.data[8]);
+        fmt::print("Success A");
+    }
 
     //Any other value, print to console
     default:
-        fmt::print("Error!, didn't find pack state of charge or voltage\n");
+        fmt::print("Error!, didn't find pack state of charge or voltatge\n")
         fmt::print("ID: 0x{:02X}, Ext: {}, RTR: {}, Err: {}, Payload: 0x{:02X} 0x{:02X} 0x{:02X} "
             "0x{:02X} 0x{:02X} 0x{:02X} 0x{:02X} 0x{:02X}\n",
             CAN::frameId(frame),
@@ -81,7 +106,6 @@ void BMS::newFrame(const can_frame& frame) {
             frame.data[6],
             frame.data[7]);
     }
-
 }
 
 void BMS::newError(const can_frame&) {
@@ -89,3 +113,5 @@ void BMS::newError(const can_frame&) {
 };
 
 void BMS::newTimeout(){};
+
+void BMS::recieveData() {};
