3cd2565980acc53716e4ad1424a607a9ca94eed1
Dallas Hart <nomalahvids@gmail.com>
2022-11-17 14:19:07 -0500

    Switch to DBC decoding

--- src/Interfaces/Full/BMS.hpp
+++ src/Interfaces/Full/BMS.hpp
@@ -1,24 +1,32 @@
 #pragma once
 
 #include <QObject>
+#include <fstream>
+#include <unordered_map>
 
 #include <Interface.hpp>
+#include <dbcppp/Network.h>
 
 class BMS : public QObject, public CAN::Interface {
     Q_OBJECT
   public:
-    BMS(QObject* parent = nullptr) : QObject(parent) {
+    BMS(QObject* parent = nullptr) : QObject(parent), dbc_network(dbcppp::INetwork::LoadNetworkFromFile("Orion_CANBUS.dbc")) {
         this->CAN::Interface::startReceiving(
             "can0", BMS::filters, BMS::num_of_filters, BMS::timeout_ms);
+        
+        for (const dbcppp::IMessage& msg : net->Messages()) {
+            messages.insert(std::make_pair(msg.Id(), &msg));
+        }
+        
+        can_signal_dispatch["Pack_Open_Voltage"]    = &newAccumulatorOpenVoltage;
+        can_signal_dispatch["Pack_SOC"]             = &newAccumulatorSOC;
+        can_signal_dispatch["Pack_Inst_Voltage"]    = &newAccumulatorInstVoltage;
+        can_signal_dispatch["Pack_Current"]         = &newAccumulatorCurrent;
+        can_signal_dispatch["High_Temperature"]     = &newAccumulatorMaxTemp;
+        can_signal_dispatch["Internal_Temperature"] = &newBMSTemp;
     }
 
     ~BMS() = default;
-
-
-    float toCelsius(uint8_t byte);
-    float toStateOfCharge(uint8_t byte);
-    float toCurrent(uint8_t low_byte, uint8_t high_byte);
-    float toVoltage(uint8_t low_byte, uint8_t high_byte);
 
   signals:
     void newBMSTemp(float temp);
@@ -36,6 +44,10 @@ class BMS : public QObject, public CAN::Interface {
     void newTimeout() override;
 
   private:
+    static dbcppp::INetwork dbc_network;
+    static std::unordered_map<uint64_t, const dbcppp::IMessage*> can_messages;
+    static std::unordered_map<std::string, void (*)(float)> can_signal_dispatch;
+
     static constexpr size_t num_of_filters = 3;
     inline static can_filter filters[num_of_filters] = {
         {
